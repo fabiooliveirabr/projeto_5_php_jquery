9047d60a376c160f86f87ee1a9de4865287fcfc9
fabiooliveirabr <136032550+fabiooliveirabr@users.noreply.github.com>
2024-11-11 20:12:38 -0300

    Update bd_prefeitura_sp.sql

--- bd_prefeitura_sp.sql
+++ bd_prefeitura_sp.sql
@@ -49,18 +49,6 @@ INSERT INTO `tb_protocolos` (`num_protocolo`, `nome_completo`, `cpf`, `email`, `
 (10002, 'FABIO DE OLIVEIRA SILVA', '23123234240', 'fabiodeoliveirabr@gmail.com', '(81) 92832-2939', 'Infraestrutura', 'Arvore com risco de cair', '2024-11-08 21:31:00'),
 (10003, 'FABIO DE OLIVEIRA SILVA', '23123234240', 'fabiodeoliveirabr@gmail.com', '(81) 92832-2939', 'Infraestrutura', 'Arvore com risco de cair', '2024-11-08 21:32:29'),
 (10004, 'FABIO DE OLIVEIRA SILVA', '23123234240', 'fabiodeoliveirabr@gmail.com', '(81) 92832-2939', 'Infraestrutura', 'Arvore com risco de cair', '2024-11-08 21:35:36'),
-(10005, 'FABIO DE OLIVEIRA SILVA', '09090', 'fabiodeoliveirabr@gmail.com', '8196337119', 'Finanças', 'aksaksl', '2024-11-08 21:52:46'),
-(10006, 'FABIO DE OLIVEIRA SILVA', '232323', 'fabiodeoliveirabr@gmail.com', '8196337119', 'Administração', '3skdlksdlk', '2024-11-08 21:53:31'),
-(10007, 'FABIO DE OLIVEIRA SILVA', 'wqqwqw', 'fabiodeoliveirabr@gmail.com', '8196337119', 'Finanças', 'zkdkzjk', '2024-11-08 21:56:01'),
-(10008, 'FABIO DE OLIVEIRA SILVA', 'wqqwqw', 'fabiodeoliveirabr@gmail.com', '8196337119', 'Finanças', 'zkdkzjk', '2024-11-08 21:56:34'),
-(10009, 'FABIO DE OLIVEIRA SILVA', 'wqqwqw', 'fabiodeoliveirabr@gmail.com', '8196337119', 'Finanças', 'zkdkzjk', '2024-11-08 21:56:45'),
-(10010, 'Fábio de Oliveira e Silva fabiodeoliveirabr', '2309', 'fabiodeoliveirabr@gmail.com', '81992883019', 'Finanças', 'plsadklas', '2024-11-08 21:57:23'),
-(10011, 'Fábio de Oliveira e Silva fabiodeoliveirabr', '2309', 'fabiodeoliveirabr@gmail.com', '81992883019', 'Finanças', 'plsadklas', '2024-11-08 21:57:32'),
-(10012, 'Fábio de Oliveira e Silva fabiodeoliveirabr', '2309', 'fabiodeoliveirabr@gmail.com', '81992883019', 'Finanças', 'plsadklas', '2024-11-09 08:09:43'),
-(10013, 'Fábio de Oliveira e Silva fabiodeoliveirabr', '4444', 'fabiodeoliveirabr@gmail.com', '81992883019', 'Administração', '343434', '2024-11-11 18:39:29'),
-(10014, 'Fábio de Oliveira e Silva fabiodeoliveirabr', '34355', 'fabiodeoliveirabr@gmail.com', '81992883019', 'Administração', '3343434', '2024-11-11 18:41:45'),
-(10015, 'Fábio de Oliveira e Silva fabiodeoliveirabr', '454545', 'fabiodeoliveirabr@gmail.com', '81992883019', 'Finanças', '343434', '2024-11-11 18:48:54'),
-(10016, 'FABIO DE OLIVEIRA SILVA', '66556565', 'fabiodeoliveirabr@gmail.com', '8196337119', 'Administração', 'lskdl klkals klaksl aka', '2024-11-11 19:47:26');
 
 --
 -- Indexes for dumped tables
